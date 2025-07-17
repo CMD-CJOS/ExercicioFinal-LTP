4af0ce5891f090129fd1bd6d45e34262bf1be841
Carlos Souza <junio200929@gmail.com>
2025-07-17 11:31:20 -0300

    Update Exercicio27.cpp

--- Exercicio27/Exercicio27.cpp
+++ Exercicio27/Exercicio27.cpp
@@ -4,3 +4,33 @@
 //Atividade: Lista de Exercícios 2 - Exercício 27 
 #include <stdio.h>  
 #define _CRT_SECURE_NO_WARNINGS  
+
+
+/*  27.	Elabore uma função que receba dois números inteiros como parâmetros e retorne o maior entre eles. 
+        No programa principal, leia dois números inteiros do usuário e, utilizando a função, exiba qual deles é o maior.
+ 
+*/
+
+int maior(int a, int b) {  
+    if (a > b) {  
+        return a;  
+    } else {  
+        return b;  
+    }  
+}
+
+int main() {  
+    int num1, num2;  
+    
+    printf("Digite o primeiro numero: ");  
+    scanf_s("%d", &num1);  
+    
+    printf("Digite o segundo numero: ");  
+    scanf_s("%d", &num2);  
+    
+    int resultado = maior(num1, num2);  
+    
+    printf("O maior numero e: %d\n", resultado);  
+    
+    return 0;  
+}
