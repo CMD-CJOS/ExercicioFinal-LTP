bd62e38fe6df52159531d1dc7eb7ae814e947dd5
Carlos Souza <junio200929@gmail.com>
2025-07-17 11:31:07 -0300

    Update Exercicio26.cpp

--- Exercicio26/Exercicio26.cpp
+++ Exercicio26/Exercicio26.cpp
@@ -4,3 +4,29 @@
 //Atividade: Lista de Exercícios 2 - Exercício 26 
 #include <stdio.h>  
 #define _CRT_SECURE_NO_WARNINGS  
+
+
+/* 26.	Crie uma função que receba como parâmetro um número inteiro positivo N e retorne o seu fatorial. 
+        No programa principal, leia um número N do usuário, chame a função para calcular o fatorial e exiba o resultado.
+*/
+
+int fatorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * fatorial(n - 1);
+    }
+}
+
+int main() {
+    int n;
+    printf("Digite um numero inteiro positivo: ");
+    scanf_s("%d", &n);
+    if (n < 0) {
+        printf("Fatorial nao definido para numeros negativos.\n");
+    } else {
+        int resultado = fatorial(n);
+        printf("O fatorial de %d e: %d\n", n, resultado);
+    }
+    return 0;
+}
