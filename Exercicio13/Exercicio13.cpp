a848a6ceda5d10c6ca2f85d9bbc215583d6834f6
Carlos Souza <junio200929@gmail.com>
2025-07-17 11:27:19 -0300

    Update Exercicio13.cpp

--- Exercicio13/Exercicio13.cpp
+++ Exercicio13/Exercicio13.cpp
@@ -4,3 +4,31 @@
 //Atividade: Lista de Exercícios 2 - Exercício 13  
 #include <stdio.h>  
 #define _CRT_SECURE_NO_WARNINGS  
+
+
+/* 13.	Faça um programa que receba um número inteiro não negativo e calcule o seu fatorial. 
+*/
+
+
+int main() {  
+	int num, fatorial = 1;
+	int numorig;
+
+	printf("Digite um numero inteiro nao negativo: ");
+	scanf_s("%d", &num);
+	numorig = num;
+
+	while (num > 1 ) {
+
+		if (num == 0 || num == 1) {
+			fatorial = 1;
+			break;
+		}
+		 else {
+			fatorial = num * fatorial;
+		}
+		num--;
+	}
+	printf("Fatorial do numero %d: %d\n", numorig, fatorial);
+	return 0;
+}
