f1cadd263e0ee6f59f90ea6912f38d18c495c5a1
Carlos Souza <junio200929@gmail.com>
2025-07-17 11:29:54 -0300

    Update Exercicio21.cpp

--- Exercicio21/Exercicio21.cpp
+++ Exercicio21/Exercicio21.cpp
@@ -4,3 +4,33 @@
 //Atividade: Lista de Exercícios 2 - Exercício 21  
 #include <stdio.h>  
 #define _CRT_SECURE_NO_WARNINGS  
+
+
+
+/* 21.	Faça um programa que leia um vetor de 8 números inteiros. 
+        Em seguida, o programa deve contar e exibir quantos desses números são pares e quantos são ímpares.         
+*/
+
+
+
+int main() {
+    int vetor[8], i, pares = 0, impares = 0;
+
+    for (i = 0; i < 8; i++) {
+        printf("Digite o %d numero: ", i + 1);
+        scanf_s("%d", &vetor[i]);
+    }
+
+    for (i = 0; i < 8; i++) {
+        if (vetor[i] % 2 == 0) {
+            pares++;	
+        } else {
+            impares++;
+        } 
+        
+    }
+ 
+    printf("Quantidade de numeros pares: %d\n", pares);
+    printf("Quantidade de numeros impares: %d\n", impares);
+    return 0;
+}
