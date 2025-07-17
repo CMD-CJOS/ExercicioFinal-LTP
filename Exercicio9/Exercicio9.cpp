d4c0942c01f1946245f8b014c19762571e3e2f47
Carlos Souza <junio200929@gmail.com>
2025-07-17 11:32:32 -0300

    Update Exercicio9.cpp

--- Exercicio9/Exercicio9.cpp
+++ Exercicio9/Exercicio9.cpp
@@ -5,3 +5,20 @@
 #include <stdio.h>  
 #define _CRT_SECURE_NO_WARNINGS  
 
+/* 9.	Faça um programa em  que leia valores numéricos até que um zero seja lido e mostre a quantidade de números lidos.
+*/
+
+int main() {
+	int numero = 1; 
+	int contador = 0; 
+
+while (numero != 0) {
+		printf("Digite um numero (0 para sair): ");
+		scanf_s("%d", &numero);
+		if (numero != 0) {
+			contador++;
+		}
+	}
+	printf("Quantidade de numeros lidos: %d\n", contador);
+	return 0;
+}
